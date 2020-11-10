b13061e971c1bdbd3d7d0e6c7b3d4b1a83b16314
eightbite <71624120+eightbite@users.noreply.github.com>
2020-11-10 21:04:59 +0900

    DBにデータ保存

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,7 +4,22 @@ class RoomsController < ApplicationController
   end
    
   def new
+    @room = Room.new
   end
+
   def create
+    @room = Room.new(rooms_parameters)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def rooms_parameters
+    params.require(:room).permit(:name, :purpose_id, :password).merge(user_id: current_user.id)
+  end
+
 end
