d61c4fa9b61360e9d3f4860cb82e5f9a9ec83c5d
eightbite <71624120+eightbite@users.noreply.github.com>
2020-11-10 21:02:30 +0900

    active_hashの導入

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,10 @@
 class Room < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :purpose
+
   belongs_to :user
 
   validates :name, presence: true
+  validates :purpose_id, numericality: { other_than: 0 }
+  
 end
