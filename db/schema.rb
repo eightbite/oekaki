733ab9da71fcb1d9785d0b68842f4de90f26582c
eightbite <71624120+eightbite@users.noreply.github.com>
2020-11-12 15:05:39 +0900

    ルーム参加機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2020_11_10_101552) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name", null: false
-    t.string "password"
     t.integer "purpose_id", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_rooms_on_user_id"
